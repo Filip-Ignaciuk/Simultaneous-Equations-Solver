625863fbff48967866561594c5ae43ad26eeecbd
Filip Ignaciuk <Ignaciuk.Filip@gmail.com>
2021-02-23 16:17:07 +0000

    Add project files.

---
+++ Simultaneous Equations Solver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simultaneous_Equations_Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
