de6d48cad06b65b9b2b31491669c20db508be4ca
Filip Ignaciuk <Ignaciuk.Filip@gmail.com>
2021-02-23 21:11:20 +0000

    Managed to figuare out how to extract the 3 parts out of a equation! For example 2x + 3y = 5. Part 1 would be 2, part 2 3 and part 3 5. All of this with only putting the equation.

--- Simultaneous Equations Solver/Program.cs
+++ Simultaneous Equations Solver/Program.cs
@@ -2,8 +2,27 @@
 
 namespace Simultaneous_Equations_Solver
 {
+    static class Helper
+    {
+        public static string GetUntilOrEmpty(this string text, string stopAt)
+        {
+            if (!String.IsNullOrWhiteSpace(text))
+            {
+                int charLocation = text.IndexOf(stopAt, StringComparison.Ordinal);
+
+                if (charLocation > 0)
+                {
+                    return text.Substring(0, charLocation);
+                }
+            }
+
+            return String.Empty;
+        }
+    }
+
     class Program
     {
+
         string equation1;
         string equation2;
 
@@ -18,12 +37,37 @@ namespace Simultaneous_Equations_Solver
             string part5;
             string part6;
 
-            if ((_equation1.Contains(" + ") && _equation2.Contains(" + ")) || (_equation1.Contains(" - ") && _equation2.Contains(" - ")))
+            if ((_equation1.Contains(" + ") && _equation2.Contains(" + ")))
             {
                 _equation1 = _equation1.Replace("x", "");
                 _equation1 = _equation1.Replace("y", "");
                 _equation1 = _equation1.Replace(" ", "");
-                Console.WriteLine(_equation1);
+
+                _equation2 = _equation2.Replace("x", "");
+                _equation2 = _equation2.Replace("y", "");
+                _equation2 = _equation2.Replace(" ", "");
+
+                part1 = _equation1.GetUntilOrEmpty("+");
+                Console.WriteLine(part1);
+
+
+                string _part2 = _equation1.GetUntilOrEmpty("=");
+                part2 = _part2.Replace(part1 + "+", "");
+                Console.WriteLine(part2);
+
+
+
+                part3 = _equation1.Substring(_equation1.IndexOf("="));
+                part3 = part3.Replace("=", "");
+                Console.WriteLine(part3);
+
+                part6 = _equation2.Substring(_equation2.IndexOf("="));
+                part6 = part6.Replace("=", "");
+                Console.WriteLine(part6);
+            }
+            else if (_equation1.Contains(" - ") && _equation2.Contains(" - "))
+            {
+
             }
             else if (_equation1.Contains(" - ") && _equation2.Contains(" + "))
             {
@@ -33,13 +77,12 @@ namespace Simultaneous_Equations_Solver
             {
 
             }
-
         }
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello welcome to Simultaneous_Equations_Solver, to use this please make sure that there are spaces betweem +,-,+. For example 2x - 3y = 5 NOT 2x-3y=5");
+            Console.WriteLine("Hello welcome to Simultaneous_Equations_Solver, to use this please make sure that there are spaces betweem +,-,+. For example 2x + 3y = 5, NOT 2x-3y=5");
             Console.Write("First equation:");
             string equation1 = Console.ReadLine();
             Console.Write("Secound equation:");
